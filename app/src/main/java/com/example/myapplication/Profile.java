b35723e17f7b5fc5d2c92c63a930b664431763e5
Judithlulala <xl2792>
2019-05-07 23:03:20 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Profile.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Profile extends AppCompatActivity {
+
+}
